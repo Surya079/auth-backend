5296047fb0802f704370a09cb08c8b1d74ce6a9a
Surya079-V2 <Surya@hazhtech.com>
2024-12-01 14:30:59 +0530

    updated

--- DB/database.js
+++ DB/database.js
@@ -5,7 +5,10 @@ env.config();
 const connectDB = async () => {
   try {
     const MONGO_URI = process.env.DB_URI;
-    mongoose.connect(MONGO_URI);
+    mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Mongo DB runnnig");
   } catch (error) {
     console.log(error);
